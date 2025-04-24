f0e16192a76c300ac78d35562c015ab5b81f6769
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2025-04-24 00:47:47 -0300

    change from pure validation to express validation in AuthRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,13 +1,17 @@
 import express from "express";
-const router = express.Router();
-
+import { createValidator } from "express-joi-validation";
 import authController from "../controllers/authController.js";
 import checkToken from "../middlewares/checkToken.js";
+import { registerSchema } from "../validations/authValidations.js";
+import { loginSchema } from "../validations/authValidations.js";
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+const router = express.Router();
+const validator = createValidator({});
+
+router.post("/register", validator.body(registerSchema), authController.register);
+router.post("/login",  validator.body(loginSchema), authController.login);
 router.get("/checkuser", authController.checkUser);
 router.get("/:id", authController.getUserById);
-router.patch("/edit/:id", checkToken, authController.editUser);
+router.patch("/edit/:id",validator.body(registerSchema), checkToken, authController.editUser);
 
 export default router;
