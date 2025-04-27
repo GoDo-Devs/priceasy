1160bb97df793a4d4262fc34ecacaa2fc2f349b7
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2025-04-27 19:33:05 -0300

    feat: index update routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser'
 const app = express();
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import clientRoutes from './routes/clientRoutes.js'
 
 app.use(bodyParser.json());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
@@ -11,6 +12,7 @@ app.use(express.static("public"));
 
 app.use("/users", authRoutes);
 app.use("/products", productRoutes);
+app.use("/clients", clientRoutes);
 
 app.listen(4000, "0.0.0.0", () => {
   console.log("Backend running on port 4000");
